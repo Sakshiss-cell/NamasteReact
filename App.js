4e51edc4f8617430a100fdcffa694e67c58f00fb
unknown <sakshi5myth@gmail.com>
2023-01-10 11:11:01 +0530

    adding js file

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading1=React.createElement("h1",{id:"title"},"hello everyone1");
+const heading2=React.createElement("h1",{id:"title"},"hello everyone2");
+const div=React.createElement("div",{id:"container"},[heading1,heading2]);
+    console.log(React);
+    //assingning to root
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    console.log(ReactDOM);
+
+    // passingelement in root
+    root.render(div);
