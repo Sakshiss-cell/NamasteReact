354b39515a5b70ac3beb80fbda76f0aa491ecff9
sakshi <sakshigawande40@gmail.com>
2023-01-14 11:47:01 +0530

    first commit

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+
+
 const heading1=React.createElement("h1",{id:"title"},"hello everyone1");
 const heading2=React.createElement("h1",{id:"title"},"hello everyone2");
 const div=React.createElement("div",{id:"container"},[heading1,heading2]);
